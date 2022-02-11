78c646695da28208db22883842af0847f2ca43df
Dulatto <dulat.omarov@gmail.com>
2022-02-10 22:27:32 -0500

    questions

--- src/App.tsx
+++ src/App.tsx
@@ -51,16 +51,23 @@ const App =() =>{
     {!gameOver ? <p className="score">Score:</p> : null}
 
      {loading && <p>Loading Questions...</p> }
+     {!loading && !gameOver && (
+        <QuestionCard 
+        questionNr={number + 1}
+        totalQuestions ={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> 
+     )}
+    
+    {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ?
+    (<button className="next" onClick={nextQuestion}>
+      Next Question
+      </button>) : null
+  }
      
-     {/* <QuestionCard 
-       questionNr={number + 1}
-       totalQuestions ={TOTAL_QUESTIONS}
-       question={questions[number].question}
-       answers={questions[number].answers}
-       userAnswer={userAnswers ? userAnswers[number] : undefined}
-       callback={checkAnswer}
-     /> */}
-     <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
